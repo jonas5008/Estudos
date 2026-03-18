60213e9861ab373148e89f7f5754d088acced77f
jonas5008 <jonasquintao408@gmail.com>
2026-03-18 18:17:10 -0300

    18/03/26
    Semana 5 - Programação para Internet

--- Tec. Informatica/Programação para Internet - PPI/atividades/sorteador-numeros/app.js
+++ Tec. Informatica/Programação para Internet - PPI/atividades/sorteador-numeros/app.js
@@ -12,36 +12,56 @@ let ate = perseInt (document.getElementById("ate").value);
 
 
 
-
-
 let numero;
 let sorteados = [];
 
 for (let i = 0; < quantidade; i++){
     numero = gerarNumeroAleatorio (de, ate);
-    while sorteados.includes(numero){
+
+    while (sorteados.includes(numero){
+    numero = gerarNumeroAleatorio (de, ate);
     }
     sorteados.push(numero);
 
-    alert(sorteados)
 
-
-}
+    }
 
 
 let resultado = document.getElementById('resultado');
-resultado.innerHTML = '<label class="texto_paragrafo">Números sortados: $(sorteados)</label>';
+resultado.innerHTML = `<label class="texto_paragrafo">Números sortados: ${sorteados}</label>`
 
 
 //console.log(quantidade);
 //console.log(de);
 //console.log(ate);
 
-
+}
 
 
 function gerarNumeroAleatorio(min, max){
     return Meth.flor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function reiniciarJogo(){
+    let quantidade = document.getElementById('quantidade');
+    quantidade.innerHTML = '';
+    let de = document.getElementById('de');
+    de.innerHTML = '';
+    let ate = document.getElementById('ate');
+    ate.innerHTML='';
+}
+
+function alterarStatusbotao(){
+    let botao = document.getElementById('btn-reiniciar')
+    if(botao.classList.contains('container_botao-desabilitado)')
+
+
+
+
+
+
+
 }
 
 
