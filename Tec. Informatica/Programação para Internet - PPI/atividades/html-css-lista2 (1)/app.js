8a4c893b876811f529f52389b91fcadf6d135755
jonas5008 <jonasquintao408@gmail.com>
2026-05-05 22:14:59 -0300

    html-css-lista2
    05/05/26

---
+++ Tec. Informatica/Programação para Internet - PPI/atividades/html-css-lista2 (1)/app.js
@@ -0,0 +1,41 @@
+function ex001() {
+let texto = document.querySelector("h1");
+texto.textContent = ("Hora do Desafio");
+
+  
+}
+
+function ex002() {
+  console.log("O botão foi clicado");
+  
+}
+
+function ex003() {
+  alert("eu amo JS!");
+  
+}
+
+function ex004() {
+  let cidade = prompt("Me fale uma cidade do Brasil");
+  alert(`Estive em ${cidade} e lembrei de você`)
+  
+}
+
+function ex005() {
+  let n1 = parseInt(prompt("Fale um numero inteiro"));
+  let n2 = parseInt(prompt("Fale um numero inteiro"));
+
+  let soma = n1 + n2;
+  alert(`O resultado da soma é ${soma}`);
+  
+}
+
+function ex006() {
+  console.log("Olá, mundo!");
+  
+}
+
+function ex007() {
+  let nome = document.g
+  
+}
