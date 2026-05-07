2fd66eead930d81a7ebec42892dd12f5dd24c345
jonas5008 <jonasquintao408@gmail.com>
2026-05-06 21:41:10 -0300

    Linguagem Orientada a Objetos - lista de exercicio_2
    06/05/2026

---
+++ Tec. Informatica/Linguagem Orientada a Objetos - LOO/lista_de_exercicio_2/index.php
@@ -0,0 +1,49 @@
+<?php
+
+$numero = $argv[1];
+$nomeDaFuncao = 'ex' . $numero;
+
+echo "========================================\n";
+echo "   Executando o Exercício $numero\n";
+echo "========================================\n\n";
+
+$nomeDaFuncao(); 
+
+echo "\n========================================\n";
+
+
+// ÁREA DOS EXERCÍCIOS: Funções com a lógica de cada questão
+// ==============================================================================
+
+function ex1() {
+    echo "Olá, Mundo!\n";
+}
+
+function ex2() {
+    $nome = readline("Qual é o seu nome? ");
+    echo "Olá $nome, é um prazer te conhecer!\n";
+}
+
+function ex3() {
+    $nome = readline("Nome do Funcionário: ");
+    $salario = floatval(readline("Salário: "));
+    echo "O funcionário $nome, tem um salário de $salario, em Junho. \n";
+}
+
+function ex4() {
+    $valor1 = intval(readline("Digite um valor: "));
+    $valor2 = intval(readline("Digite outro valor: "));
+    $soma = $valor1 + $valor2;
+    echo "A soma entre $valor1, e $valor2, é igual a $soma,. \n";
+}
+
+function ex5() {
+    $nota1 = floatval(readline("Nota 1: "));
+    $nota2 = floatval(readline("Nota 2: "));
+    $media = $nota1 + $nota2 / 2;
+    echo"A média entre $nota1, e $nota2, é igual a $media,"
+}
+
+function ex6() {
+    echo "Olá, Mundo!\n";
+}
